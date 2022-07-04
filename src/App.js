f583b6e9beb03970d75c82195aedc06fa92b76d9
unknown <development.ven@gmail.com>
2022-07-04 10:54:42 -0400

    Agregar de fragmento self-closing tag y responsividad

--- src/App.js
+++ src/App.js
@@ -6,21 +6,25 @@ function App() {
     <div className="App">
       <div className='contenedor-principal'>
       <h1>Here is what our alumni say about freeCodeCamp:</h1>
+      {/* en esta tag autocerrada  debemos adicionarle en el fragmento en este caso uso la sintaxis corta
+      en este caso ingreso strong directamente
+      https://es.reactjs.org/docs/fragments.html#short-syntax */}
       <Testimonio
       nombre='Shawn Wang'
       pais='Singapore'
       imagen='shawn'
       cargo='Software Engineer'
-      testimonio='Its scary to change careers.
+      testimonio={<>'Its scary to change careers.
        I only gained confidence that I could code by working through the hundreds of hours of free lessons on freeCodeCamp. 
-       Within a year I had a six-figure job as a Software Engineer. freeCodeCamp changed my life.'
+       Within a year I had a six-figure job as a Software Engineer. <strong>freeCodeCamp changed my life.'</strong></>}
       empresa='Amazon' />
+{/* para este ejemplo agrego la classe global bold tambien aceptable */}
       <Testimonio
       nombre='Sarah Chima'
       pais='Nigeria'
       imagen='sarah'
       cargo='Software Engineer'
-      testimonio='freeCodeCamp was the gateway to my career as a software developer. The well-structured curriculum took my coding knowledge from a total beginner level to a very confident level. It was everything I needed to land my first dev job at an amazing company.'
+      testimonio={<>'<span className="bold">freeCodeCamp was the gateway to my career </span>as a software developer. The well-structured curriculum took my coding knowledge from a total beginner level to a very confident level. It was everything I needed to land my first dev job at an amazing company.'</>}
       empresa='ChatDesk' />
       <Testimonio
       nombre='Emma Bostian'
@@ -28,9 +32,9 @@ function App() {
       imagen='emma'
       cargo='Software Engineer'
       empresa='Spotify'
-      testimonio='Ive always struggled with learning JavaScript. Ive taken many courses but freeCodeCamps course was the one which stuck. 
-      Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence 
-      I needed to land my dream job as a software engineer at Spotify.' />
+      testimonio={<>'Ive always struggled with learning JavaScript. Ive taken many courses but freeCodeCamps course was the one which stuck. 
+      Studying JavaScript as well as data structures and algorithms on <span className="bold">freeCodeCamp gave me the skills and confidence </span>
+      I needed to land my dream job as a software engineer at Spotify.'</>} />
      </div>   
     </div>
   );
